12854cb18089a7a0d9ffd470ef6ddcddb51eb9e0
Mihai Georgescu <m.georgescu@emetriq.com>
2024-01-11 20:18:47 +0100

    correct gitignore

--- dockerized/simple_hyperopt/mlflow_experiments.py
+++ dockerized/simple_hyperopt/mlflow_experiments.py
@@ -222,27 +222,3 @@ if retrain_best:
           str(model).split('(')[0])
     print('\n', classification_report(y_train, y_pred_train))
     print('\n', classification_report(y_test, y_pred_test))
-
-# %%
-import mlflow
-
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_diabetes
-from sklearn.ensemble import RandomForestRegressor
-
-mlflow.sklearn.autolog()
-
-db = load_diabetes()
-X_train, X_test, y_train, y_test = train_test_split(db.data, db.target)
-
-# Create and train models.
-rf = RandomForestRegressor(n_estimators=100, max_depth=6, max_features=3)
-rf.fit(X_train, y_train)
-
-# Use the model to make predictions on the test dataset.
-predictions = rf.predict(X_test)
-
-
-# %%
-
-# %%
